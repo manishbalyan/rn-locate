f637491a62e3333f178ab6a0cd33cba340d882c0
manishbalyan <balyan05.manish@gmail.com>
2018-06-21 00:20:16 +0530

    redux implemeted

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import { AppRegistry } from 'react-native';
+import React from 'react';
 import App from './App';
-AppRegistry.registerComponent('rncourse', () => App);
+import { Provider } from 'react-redux'
+import configureStore from './src/store/configureStore'
+
+
+const store = configureStore();
+const RNRedux = () => (
+    <Provider store={store}>
+        <App />
+    </Provider >
+)
+AppRegistry.registerComponent('rncourse', () => RNRedux);
---
+++ src/store/configureStore.js
@@ -0,0 +1,12 @@
+import { combineReducers, createStore } from 'redux'
+import placesReducer from './reducers/placesReducer'
+
+const rootReducer = combineReducers({
+    places: placesReducer
+})
+
+const configureStore = () => {
+    return createStore(rootReducer);
+}
+
+export default configureStore;
